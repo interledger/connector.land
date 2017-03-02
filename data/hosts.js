3dec93fce66060ff9caae409daf37c9e2de4eb55
Michiel de Jong <michiel@ripple.com>
2017-03-02 16:49:10 +0100

    Move data/hosts.js to this github repo

---
+++ data/hosts.js
@@ -0,0 +1,43 @@
+module.exports.hosts = [
+  { "hostname": "red.ilpdemo.org",                        "owner": ""},
+  { "hostname": "blue.ilpdemo.org",                       "owner": ""},
+  { "hostname": "kes-ilp.abdishakur.com",                 "owner": "abdishakur"},
+  { "hostname": "ilp.bigchaindb.com",                     "owner": "dimitri"},
+  { "hostname": "zar-ilp.hopebailie.com",                 "owner": "adrian"},
+  { "hostname": "milton.ilp.network",                     "owner": "javi"},
+  { "hostname": "pineapplesheep.ilp.rocks",               "owner": "pineapplesheep"},
+  { "hostname": "usd.interledger.network",                "owner": "winthan"},
+  { "hostname": "mmk.interledger.network",                "owner": "nyeinminn"},
+  { "hostname": "interledger.kr",                         "owner": "mspark"},
+  { "hostname": "nexus.justmoon.com",                     "owner": "stefan"},
+  { "hostname": "gordon.mearnag.org",                     "owner": "connie"},
+  { "hostname": "coins.paleorbglow.com",                  "owner": "kanaan"},
+  { "hostname": "cornelius.sharafian.com",                "owner": "sharafian"},
+  { "hostname": "cygnus.vahehovhannisyan.com",            "owner": "vahe"},
+  { "hostname": "grifiti.web-payments.net",               "owner": "pkrey"},
+  { "hostname": "hive.dennisappelt.com",                  "owner": "dennis"},
+  { "hostname": "payment.am",                             "owner": "mesrop"},
+  { "hostname": "ilp-kit.michielbdejong.com",             "owner": "michiel"},
+  { "hostname": "john.jpvbs.com",                         "owner": ""},
+  { "hostname": "best-ilp.herokuapp.com",                 "owner": ""},
+  { "hostname": "someledger.herokuapp.com",               "owner": ""},
+  { "hostname": "michiel-is-not-available.herokuapp.com", "owner": "admin"},
+  { "hostname": "royalcrypto.com",                        "owner": "twarden"},
+  { "hostname": "ggizi.herokuapp.com",                    "owner": ""},
+  { "hostname": "michiel-eur.herokuapp.com",              "owner": ""},
+  { "hostname": "ilp.hexdivision.com",                    "owner": "admin"},
+//  { "hostname": "",                                       "owner": ""},
+
+// ^^^ Add your ILP host by duplicating, uncommenting, and editing the line above ^^^
+
+// # hostname
+// This should have the format like 'example.com', so without the 'https://' in front.
+
+// # owner
+// The main user account you use on your ledger. People can use this to, for instance, send test payments to your
+// node. It's also a sort of indication of what your nickname on other platforms like github or irc might be,
+// so in the current situation where most host admins are also ILP enthusiasts, it gives us a hint of who to
+// contact if a node is down. If you haven't created any users yet, and the software you run is ilp-kit, then
+// you can simply fill in 'admin', which is the ledger user that always exists when you first install the ilp-kit
+// software on your server.
+];
